89ce9a5177f6681db8c8594c814b4bf7b5e01ee4
Randy Singh <rsingh@infusion.com>
2015-06-16 15:02:27 -0400

    Adding demo

---
+++ DemoApp/DemoApp/DemoApp.Common/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Caliburn.Micro;
+
+namespace DemoApp.Common.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+        public string Name { get; set; }
+        public MainViewModel()
+        {
+            Name = "Hello from portable";
+        }
+
+    }
+}
---
+++ DemoApp/DemoApp/DemoApp.Windows/App.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml.Controls;
+using Caliburn.Micro;
+using DemoApp.Common.ViewModels;
+using DemoApp.Windows.Views;
+
+namespace DemoApp.Windows
+{
+    public sealed partial class App
+    {
+        private WinRTContainer _container;
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override IEnumerable<Assembly> SelectAssemblies()
+        {
+
+            return new[]
+                {
+                    GetType().GetTypeInfo().Assembly,
+                    typeof (MainViewModel).GetTypeInfo().Assembly
+                };
+        }
+
+        protected override void Configure()
+        {
+            MessageBinder.SpecialValues.Add("$clickeditem", c => ((ItemClickEventArgs)c.EventArgs).ClickedItem);
+
+            ViewModelLocator.AddNamespaceMapping("DemoApp.Windows.Views", "DemoApp.Common.ViewModels");
+           
+            
+            _container = new WinRTContainer();
+
+            _container.RegisterWinRTServices();
+
+            _container.PerRequest<MainViewModel>();
+
+            PrepareViewFirst();
+        }
+
+        protected override void PrepareViewFirst(Frame rootFrame)
+        {
+            _container.RegisterNavigationService(rootFrame);
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            Initialize();
+
+            if (args.PreviousExecutionState == ApplicationExecutionState.Running)
+                return;
+
+            if (RootFrame.Content == null)
+                DisplayRootView<MainView>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
