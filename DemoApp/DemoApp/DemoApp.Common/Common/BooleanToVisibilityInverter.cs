72df931c6584d967df5504dacae20933405ee6e3
Randy Singh <rsingh@infusion.com>
2015-06-17 10:51:30 -0400

    Adding some converters and ResourceDictionary

---
+++ DemoApp/DemoApp/DemoApp.Common/Common/BooleanToVisibilityInverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace DemoApp.Common.Common
+{
+    public class BooleanToVisibilityInverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return !((value is bool && (bool)value)) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
