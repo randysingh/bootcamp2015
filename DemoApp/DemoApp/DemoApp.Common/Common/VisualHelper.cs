9cc3a137481608642e20131b5ae987988412d92e
Randy Singh <rsingh@infusion.com>
2015-06-17 07:50:58 -0400

    Adding in VisualHelper and VisualStateBehavior

---
+++ DemoApp/DemoApp/DemoApp.Common/Common/VisualHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace DemoApp.Common.Common
+{
+    public class VisualHelper
+    {
+        public static T GetParentOfT<T>(DependencyObject parent) where T : DependencyObject
+        {
+            while (parent != null)
+            {
+                if (parent is T)
+                {
+                    return (T)parent;
+                }
+
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return null;
+        }
+
+        //Only executes if an object has not been loaded.
+        public static bool ExecuteOnLoaded(FrameworkElement element, Action<RoutedEventArgs> onLoaded)
+        {
+            RoutedEventHandler loaded = null;
+            loaded = (s, e) =>
+            {
+                onLoaded(e);
+                element.Loaded -= loaded;
+            };
+
+            element.Loaded += loaded;
+            return false;
+        }
+    }
+}
---
+++ DemoApp/DemoApp/DemoApp.Windows/Common/VisualStateBehavior.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using DemoApp.Common.Common;
+using Microsoft.Xaml.Interactivity;
+
+namespace DemoApp.Windows.Common
+{
+    public class VisualStateBehavior : DependencyObject, IBehavior
+    {
+        public string VisualState
+        {
+            get { return (string)GetValue(VisualStateProperty); }
+            set { SetValue(VisualStateProperty, value); }
+        }
+
+        public static readonly DependencyProperty VisualStateProperty =
+            DependencyProperty.Register("VisualState", typeof(string), typeof(VisualStateBehavior), new PropertyMetadata(null, OnVisualStateChanged));
+
+        private static void OnVisualStateChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var behavior = (VisualStateBehavior)dependencyObject;
+            behavior.ApplyVisualState();
+        }
+
+        private bool _waitingToApplyState;
+
+        private void ApplyVisualState()
+        {
+            if (!string.IsNullOrEmpty(VisualState))
+            {
+                //Associated object is null, lets wait until it's not null
+                if (AssociatedObject == null)
+                {
+                    _waitingToApplyState = true;
+                    return;
+                }
+
+                var page = VisualHelper.GetParentOfT<Control>(AssociatedObject);
+                if (page == null)
+                {
+                    // Page may not be loaded yet
+                    VisualHelper.ExecuteOnLoaded((FrameworkElement)AssociatedObject, r =>
+                    {
+                        page = VisualHelper.GetParentOfT<Control>(AssociatedObject);
+                        if (page != null)
+                        {
+                            VisualStateManager.GoToState(page, VisualState, true);
+                        }
+                    });
+                }
+                else
+                {
+                    VisualStateManager.GoToState(page, VisualState, true);
+                }
+            }
+        }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            if ((associatedObject != AssociatedObject) && !DesignMode.DesignModeEnabled)
+            {
+                if (AssociatedObject != null)
+                {
+                    throw new InvalidOperationException("Cannot attach behavior multiple times.");
+                }
+
+                AssociatedObject = associatedObject;
+
+                if (_waitingToApplyState)
+                {
+                    _waitingToApplyState = false;
+                    ApplyVisualState();
+                }
+            }
+        }
+
+        public void Detach()
+        {
+        }
+
+        public DependencyObject AssociatedObject { get; private set; }
+    }
+
+
+}
